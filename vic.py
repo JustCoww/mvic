72e988c95a999261784dab4ddf637bfd6097419c
justcow <68345611+JustCoww@users.noreply.github.com>
2022-02-27 15:41:20 +0000

    Update vic.py

--- vic.py
+++ vic.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-def thumbnail(cover):
+def thumbnail(cover, output):
 
     from PIL import Image, ImageEnhance, ImageFilter
     from math import trunc
 
     # Import
-    print('Opening cover file...')
     tb = Image.open(cover)
     cv = Image.open(cover)
 
     # Cover
-    print('Resizing cover...')
     x, y = (626, 626)
     cv = cv.resize((x, y), resample=0, box=None)
     
     # Thumb Resize
-    print('Resizing thumbnail background...')
     X, Y = (1920, 1080)
     tb = tb.resize((X, Y), resample=0, box=None)
     
     # Blur and brightness
-    print('Adding blur and lowering brightness...')
     tb = ImageEnhance.Brightness(tb).enhance(0.3)
     tb = tb.filter(ImageFilter.GaussianBlur(60))
     tb = tb.copy()
 
     # Mix into file
-    print('Merging cover with background...')
     center = (trunc((X-x)/2), trunc((Y-y)/2))
     tb.paste(cv, center)
-    
-    # Export
-    print('Exporting...')
-    tb.save('thumb.png')
+    tb.save(output)
     
     return print('Thumbnail image: Done')
     
-def video(cover, song, artist):
+def video(cover, song, artist, output):
 
     from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
     from math import trunc
@@ -45,7 +37,6 @@ def video(cover, song, artist):
     X, Y = (7680, 4320) # Background size
 
     # Import cover
-    print('Opening cover file...')
     bg = Image.open(cover)
     cv = Image.open(cover)
 
@@ -58,11 +49,11 @@ def video(cover, song, artist):
     square = Image.new(mode = "RGBA", size = (2500, 2500), color = (0, 0, 0))
     bg.paste(square, (trunc((X-x)/2), 659))
     # Blur and turn brightness down
-    print('Adding blur and lowering brightness...')
+    print('Adding blur to background...')
     bg = ImageEnhance.Brightness(bg).enhance(0.4)
     bg = bg.filter(ImageFilter.GaussianBlur(120))
     # Paste cover into the background
-    print('Merging cover with background...')
+    print('Merging cover with background')
     bg.paste(cv, ( trunc((X-x)/2 ), 659))
 
 
@@ -77,13 +68,13 @@ def video(cover, song, artist):
 
     # Generate the text
     for i in [toptxt, song, artist]:
-        print(f'Writing: {i[0]}...')
+        print('Writing:', i[0])
         font = ImageFont.truetype(i[2], i[3])
         x, y = text.textsize(i[0], font=font)
         text.text(((X-x)/2, i[1]), i[0], fill=(255, 255, 255), font=font, align='center')
 
     # Export final thing to a file
     print('Exporting...')
-    bg.save('video.png')
+    bg.save(output)
 
     return print('Video image: Done')
